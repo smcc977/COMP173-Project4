2195a98a117e562512bdf3ea8198a6b8db2f385d
smcc977 <sammcc977@gmail.com>
2026-04-24 15:59:08 -0700

    Program structure

--- mmu.c
+++ mmu.c
@@ -11,6 +11,18 @@ gcc -pthread mmu.c -o mmu
 #include <sys/stat.h>
 #include <stdbool.h>
 
+/*
+TLB is array of struct of page number and frames
+Page table is array of frame number (index is page number)
+Main Memory is array of value (index is frame #)
+
+Program Flow:
+Read address
+Search TLB
+If fault search page table
+If fault allocate new frame in memory
+Return value
+*/
 
 int main(int argc, char *argv[]){    
 	return 0;
