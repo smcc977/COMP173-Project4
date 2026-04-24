755e1ba2cd8f157351c03b922dad49780e598f8b
smcc977 <sammcc977@gmail.com>
2026-04-24 15:50:12 -0700

    Base files

---
+++ mmu.c
@@ -0,0 +1,17 @@
+/*
+Complie with:
+gcc -pthread mmu.c -o mmu
+*/
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+
+int main(int argc, char *argv[]){    
+	return 0;
+}
